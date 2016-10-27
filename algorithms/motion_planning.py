d95b80eb9b6da98852cd421edf0e92be488eca5d
ballerin <francesco@ballerin.it>
2016-10-27 23:23:53 +0200

    Cleaned code and implemented wall functionality for dijkstra algorithm

--- algorithms/motion_planning.py
+++ algorithms/motion_planning.py
@@ -7,8 +7,8 @@ def dijkstra(start, end ,mat):
     It returns the distance between the starting cell and the final cell.
     '''
 
-    visited = np.zeros((len(mat),len(mat[0])))
-    dist = np.zeros((len(mat),len(mat[0])))
+    visited = np.zeros((np.shape(mat)[0],np.shape(mat)[1]))
+    dist = np.zeros((np.shape(mat)[0],np.shape(mat)[1]))
     q = queue.PriorityQueue()
     q.put((0,start))
     visited[start[0]][start[1]] = 1
@@ -16,37 +16,36 @@ def dijkstra(start, end ,mat):
     while(not q.empty()):
         top = q.get()
         val = top[0]
-        node = top[1]
-        if(val <= dist[node[0]][node[1]]):
+        node = (top[1][0],top[1][1])
+        if(val <= dist.item(node)):
             if(node[0]>2):
-                new = [node[0]-2,node[1]]
-                wall = [node[0]-1,node[1]]
-                if (not mat[wall[0]][wall[1]] and (not visited[new[0]][new[1]] or dist[new[0]][new[1]] > dist[node[0]][node[1]] +1)):
-                    dist[new[0]][new[1]] = dist[node[0]][node[1]] +1
-                    q.put((dist[new[0]][new[1]],(new[0],new[1])))
-                    visited[new[0]][new[1]] = True
+                new = (node[0]-2,node[1])
+                wall = (node[0]-1,node[1])
+                if (not mat.item(wall) and (not visited.item(new) or dist.item(new) > dist.item(node) +1)):
+                    dist.itemset(new, dist.item(node) +1)
+                    q.put((dist.item(new),new))
+                    visited.itemset(new, True)
             if(node[1]>2):
-                new = [node[0],node[1]-2]
-                wall = [node[0],node[1]-1]
-                if (not mat[wall[0]][wall[1]] and (not visited[new[0]][new[1]] or dist[new[0]][new[1]] > dist[node[0]][node[1]] +1)):
-                    dist[new[0]][new[1]] = dist[node[0]][node[1]] +1
-                    q.put((dist[new[0]][new[1]],(new[0],new[1])))
-                    visited[new[0]][new[1]] = True
-            if(len(mat)-node[0]>2):
-                new = [node[0]+2,node[1]]
-                wall = [node[0]+2,node[1]]
-                if (not mat[wall[0]][wall[1]] and (not visited[new[0]][new[1]] or dist[new[0]][new[1]] > dist[node[0]][node[1]] +1)):
-                    dist[new[0]][new[1]] = dist[node[0]][node[1]] +1
-                    q.put((dist[new[0]][new[1]],(new[0],new[1])))
-                    visited[new[0]][new[1]] = True
-            if(len(mat[0])-node[1]>2):
-                new = [node[0],node[1]+2]
-                wall = [node[0],node[1]+2]
-                if (not mat[wall[0]][wall[1]] and (not visited[new[0]][new[1]] or dist[new[0]][new[1]] > dist[node[0]][node[1]] +1)):
-                    dist[new[0]][new[1]] = dist[node[0]][node[1]] +1
-                    q.put((dist[new[0]][new[1]],(new[0],new[1])))
-                    visited[new[0]][new[1]] = True
+                new = (node[0],node[1]-2)
+                wall = (node[0],node[1]-1)
+                if (not mat.item(wall) and (not visited.item(new) or dist.item(new) > dist.item(node) +1)):
+                    dist.itemset(new, dist.item(node) +1)
+                    q.put((dist.item(new),new))
+                    visited.itemset(new, True)
+            if(np.shape(mat)[0]-node[0]>2):
+                new = (node[0]+2,node[1])
+                wall = (node[0]+1,node[1])
+                if (not mat.item(wall) and (not visited.item(new) or dist.item(new) > dist.item(node) +1)):
+                    dist.itemset(new, dist.item(node) +1)
+                    q.put((dist.item(new),new))
+                    visited.itemset(new, True)
+            if(np.shape(mat)[1]-node[1]>2):
+                new = (node[0],node[1]+2)
+                wall = (node[0],node[1]+1)
+                if (not mat.item(wall) and (not visited.item(new) or dist.item(new) > dist.item(node) +1)):
+                    dist.itemset(new, dist.item(node) +1)
+                    q.put((dist.item(new),new))
+                    visited.itemset(new, True)
             #print(dist[end[0]][end[1]])
 
     return dist[end[0]][end[1]]
-    return 1
