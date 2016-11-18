434363de03bda93091e9b64e9e2145530955d704
gottardini <danielegott98@gmail.com>
2016-11-18 22:15:48 +0100

    Added sensors emulator

---
+++ simulation/sensors.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#Matrix definition
+mat = np.matrix("0 1 0 1 0 1 0; 1 0 0 0 0 0 1; 0 1 0 1 0 0 0; 1 0 0 0 0 0 1; 0 1 0 0 0 1 0; 1 0 0 0 0 0 0; 0 0 0 1 0 1 0; 1 0 0 0 0 0 1; 0 1 0 1 0 1 0")
+
+def scanWalls(pos, orient):
+    walls=[]
+    walls.append(mat.item((pos[0]-1,pos[1])))
+    walls.append(mat.item((pos[0],pos[1]+1)))
+    walls.append(mat.item((pos[0]+1,pos[1])))
+    walls.append(mat.item((pos[0],pos[1]-1)))
+    for i in range(0,(orient+1)%4):
+        walls.append(walls[0])
+        del walls[0]
+    return walls
