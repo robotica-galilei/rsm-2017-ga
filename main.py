6a20da1b068c5e8fbe5acf79b10da9721fbb4254
gottardini <danielegott98@gmail.com>
2016-11-18 22:21:19 +0100

    Prepared main.py for simulations

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 import Pyro4
 import sys
 import time
-from simulation import simulation
-
-server = Pyro4.Proxy("PYRONAME:robot.server")    # use name server object lookup uri shortcut
-
-
-wall_map = [[[3,3,1,3],[1,1,3,3],[3,0,0,0],[0,0,0,0]], #THIS WILL BE CHANGED
-    [[3,1,3,3],[3,1,3,1],[3,3,0,0],[0,0,0,0],], #Matrix containing walls informations, 4 chars string, starting from the left, going anti-clockwise
-    [[3,1,1,1],[1,3,1,1],[1,3,2,3],[2,0,0,0]], #0 - unknown, 1 - No wall, 2 - Queued wall, 3 - Wall
-    [[3,3,3,1],[3,0,0,3],[0,0,0,3],[0,0,0,0]],
-    [[0,0,0,3],[0,0,0,0],[0,0,0,0],[0,0,0,0]]]
-
-path = [(0,0,2),(1,0,2),(1,0,1),(1,1,1),(1,2,1),(1,2,0),(0,2,0),(0,2,1),(0,3,1),(0,3,0),(0,3,3),(0,2,3),(0,1,3),(0,1,2),(0,1,1),(0,2,1),(0,2,2),(1,2,2),(2,2,2)]
+import numpy as np
+import simulation.sensors as sm
+import algorithms.motion_planning as mp
 
 
-server.setRobotStatus("Waiting for start")
-server.setVictimsNumber(0)
-server.setWallMap(wall_map)
-server.setElapsedTime(0)
 
-time.sleep(3)
+if __name__ == '__main__':
+    #Global variables
+    mat = np.matrix("0 1 0 1 0 1 0; 1 0 0 0 0 0 1; 0 1 0 1 0 0 0; 1 0 0 0 0 0 1; 0 1 0 0 0 1 0; 1 0 0 0 0 0 1; 0 0 0 1 0 1 0; 1 0 0 0 0 0 1; 0 1 0 1 0 1 0")
+    pos = (1,1)
+    orientation=3
+    queue=[]
+    print(mat)
+    
+    server = Pyro4.Proxy("PYRONAME:robot.server")    # use name server object lookup uri shortcut
+    server.setRobotStatus("Waiting for start")
+    server.setVictimsNumber(0)
+    server.setMazeMap(mat.tolist())
+    #print(mat.tolist())
+    server.setElapsedTime(0)
+    
+    #Read sensors
+    walls = sm.scanWalls(pos,orientation)
+    #print(walls)
+    
+    #Rectify readings on the orientation
+    for i in range(0,3-orientation):
+        walls.append(walls[0])
+        del walls[0]
+    #print(walls)
+    
+    #Resize map and shift indexes
+    
+    
+    #print(dijkstra([1,1],[3,3],mat))
+    #available = [[7,5],[3,1]]
+    #print(best_path(1,[1,1],available,mat))
 
-
-while True:
-    server.setRobotStatus("Exploring")
-    for i in path:
-        server.setRobotPosition((i[0],i[1]))
-        server.setRobotOrientation(i[2])
-        time.sleep(0.5)
-    server.setRobotStatus("Lack of progress")
-    time.sleep(3)
