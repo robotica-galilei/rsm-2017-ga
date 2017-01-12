1925cc88187dde1891f63c962ac6b94318c1dd96
gottardini <danielegott98@gmail.com>
2017-01-12 22:40:21 +0100

    Communication between python modules and binary blobs ready

--- oi/tof.py
+++ oi/tof.py
@@ -1,8 +1,20 @@
-def readSensor(n):
-    return 42
+import subprocess
 
-def getSide(n):
-    return (readSensor(2*n),readSensor(2*n+1))
+def readSingle():
+    proc = subprocess.Popen(["blobs/i2ctof"], stdout=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    errcode = proc.returncode
+    return out.decode('UTF-8')
 
 def readAll():
-    return [getSide(0),getSide(1),getSide(2),getSide(3)]
+    proc = subprocess.Popen(["blobs/i2ctof_multiple"], stdout=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    errcode = proc.returncode
+    out=out.decode('UTF-8').split()
+    out = [ int(x) for x in out ]
+    return out
+
+def readSide(n):
+    readings=readAll()
+    print (readings[2*n],readings[2*n+1])
+
