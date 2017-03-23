0dc347fa7a502729b66ef7217e7662e068c8e51f
ballerin <francesco@ballerin.it>
2017-03-23 18:02:07 +0100

    Updated motors lib to use pins (TODO directions)

--- actuators/motors.py
+++ actuators/motors.py
@@ -1,15 +1,19 @@
+import Adafruit_BBIO.PWM as PWM
+
 class Motor:
-    def __init__(self):
-        #TODO pin configurations
+    def __init__(self, fr, fl, rr, rl):
+        self.pins = {'fr':fr, 'fl':fl, 'rr':rr, 'rl':rl}
         self.actual_l = 0
         self.actual_r = 0
 
     def setSpeedLeft(self, power):
-        #TODO set speed
+        PWM.start(self.pins['fl'], power, 25000)
+        PWM.start(self.pins['rl'], power, 25000)
         self.actual_l = power
 
     def setSpeedRight(self, power):
-        #TODO set speed
+        PWM.start(self.pins['fr'], power, 25000)
+        PWM.start(self.pins['rr'], power, 25000)
         self.actual_r = power
 
     def stopLeft(self):
