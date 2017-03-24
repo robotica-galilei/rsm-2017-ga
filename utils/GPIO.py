ca282cef42788bed79ef9a1ccb7de9105c554f7f
ballerin <francesco@ballerin.it>
2017-03-24 23:38:44 +0100

    Created GPIOError

--- utils/GPIO.py
+++ utils/GPIO.py
@@ -4,6 +4,10 @@ OUT = "out"
 IN = "in"
 exported = set()
 
+class GPIOError(Exception):
+     def __init__(self, message, errors):
+         super(GPIOError, self).__init__(message)
+         self.errors = errors
 
 def write_file(path, value):
     file = open("/sys/class/gpio/" + path, "w")
@@ -19,5 +23,5 @@ def setup(gpio, direction):
 
 def output(gpio, value):
     if gpio not in exported:
-        raise GPIOError('GPIO setup required')
+        raise GPIOError('GPIO setup required','Cannot use gpio before init')
     write_file(gpio + "/value", str(value))
