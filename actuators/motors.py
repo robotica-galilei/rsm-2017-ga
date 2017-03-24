4a017ff81291f25d93544d65f5b03ccf200d8615
ballerin <francesco@ballerin.it>
2017-03-24 16:51:02 +0100

    Added native direction support to motor library

--- actuators/motors.py
+++ actuators/motors.py
@@ -1,17 +1,53 @@
+import sys
+sys.path.append("..")
+
 import Adafruit_BBIO.PWM as PWM
+import utils.GPIO
 
 class Motor:
-    def __init__(self, fr, fl, rr, rl):
-        self.pins = {'fr':fr, 'fl':fl, 'rr':rr, 'rl':rl}
+    def __init__(self, pins):
+        """
+        pins are provided by a dictionary formatted like:
+        {
+            'fr':fr,
+            'fl':fl,
+            'rr':rr,
+            'rl':rl,
+            'dir_fr':dir_fr,
+            'dir_fl':dir_fl,
+            'dir_rr':dir_rr,
+            'dir_rl':dir_rl
+        }
+        """
+        GPIO.setup(pins['dir_fr'], GPIO.OUT)
+        GPIO.setup(pins['dir_fl'], GPIO.OUT)
+        GPIO.setup(pins['dir_rr'], GPIO.OUT)
+        GPIO.setup(pins['dir_rl'], GPIO.OUT)
+
+        self.pins = pins
         self.actual_l = 0
         self.actual_r = 0
 
     def setSpeedLeft(self, power):
+        if(power<0):
+            GPIO.output(self.pins['dir_fl'],GPIO.HIGH)
+            GPIO.output(self.pins['dir_rl'],GPIO.HIGH)
+        else:
+            GPIO.output(self.pins['dir_fl'],GPIO.LOW)
+            GPIO.output(self.pins['dir_rl'],GPIO.LOW)
+
         PWM.start(self.pins['fl'], power, 25000)
         PWM.start(self.pins['rl'], power, 25000)
         self.actual_l = power
 
     def setSpeedRight(self, power):
+        if(power<0):
+            GPIO.output(self.pins['dir_fr'],GPIO.HIGH)
+            GPIO.output(self.pins['dir_rr'],GPIO.HIGH)
+        else:
+            GPIO.output(self.pins['dir_fr'],GPIO.LOW)
+            GPIO.output(self.pins['dir_rr'],GPIO.LOW)
+
         PWM.start(self.pins['fr'], power, 25000)
         PWM.start(self.pins['rr'], power, 25000)
         self.actual_r = power
