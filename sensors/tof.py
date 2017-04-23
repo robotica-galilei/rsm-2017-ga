e191efa75309146712488f85fbb65b472c5e587a
ballerin <francesco@ballerin.it>
2017-04-23 10:57:47 +0200

    Prepared working class. Tof 200 disabled, pins and addresses defined

--- sensors/tof.py
+++ sensors/tof.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("../")
+import time
 
 import modules.tof_60 as tof_60
 import modules.tof_200 as tof_200
@@ -8,6 +9,8 @@ import config.dimensions as dim
 class tof:
     def __init__(self, pins, addresses):
         #Init routine
+        # tof_60_pins = {'60_NO' : 'gpio66', '60_NE' : 'gpio67', '60_SO' : 'gpio69', '60_SE' : 'gpio68', '60_EN' : 'gpio45', '60_ES' : 'gpio44', '60_ON' : 'gpio23', '60_OS' : 'gpio26'}
+        # tof_60_addresses = {'60_NO' : 0x20, '60_NE' : 0x21 ,'60_SO' : 0x22, '60_SE' : 0x23, '60_EN' : 0x24, '60_ES' : 0x25, '60_ON' : 0x26, '60_OS' : 0x27}
         self.sens = {}
         self.sens['NO'] = tof_60.VL6180X(pins['60_NO'], addresses['60_NO'])
         self.sens['NE'] = tof_60.VL6180X(pins['60_NE'], addresses['60_NE'])
@@ -17,13 +20,24 @@ class tof:
         self.sens['ES'] = tof_60.VL6180X(pins['60_ES'], addresses['60_ES'])
         self.sens['ON'] = tof_60.VL6180X(pins['60_ON'], addresses['60_ON'])
         self.sens['OS'] = tof_60.VL6180X(pins['60_OS'], addresses['60_OS'])
-        self.sens['N'] = tof_60.VL6180X(pins['200_N'], addresses['200_N'])
-        self.sens['S'] = tof_60.VL6180X(pins['200_S'], addresses['200_S'])
-        self.sens['E'] = tof_60.VL6180X(pins['200_E'], addresses['200_E'])
-        self.sens['O'] = tof_60.VL6180X(pins['200_O'], addresses['200_O'])
+        #self.sens['N'] = tof_60.VL6180X(pins['200_N'], addresses['200_N'])
+        #self.sens['S'] = tof_60.VL6180X(pins['200_S'], addresses['200_S'])
+        #self.sens['E'] = tof_60.VL6180X(pins['200_E'], addresses['200_E'])
+        #self.sens['O'] = tof_60.VL6180X(pins['200_O'], addresses['200_O'])
+
+        time.sleep(1)
 
         for key, item in self.sens.items():
-            item.activate()
+            print(key)
+            i = 0
+            while(i<3):
+                try:
+                    item.activate()
+                    break
+                except:
+                    i += 1
+            print(i)
+            time.sleep(0.1)
 
     def read_raw(self, string):
         #Read just the single sensor
@@ -33,7 +47,7 @@ class tof:
         #Return a dictionary with all the sensors
         all_s = {}
         for key, item in self.sens.items():
-            all_s[key] = self.read_raw[key]
+            all_s[key] = self.read_raw(key)
         return all_s
 
     def read_fix(self,dir):
