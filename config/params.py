d78247ca94c25677233b2b3167a55ee1a9c7c8fb
ballerin <francesco@ballerin.it>
2017-05-01 12:48:06 +0200

    Refactored imu class

--- config/params.py
+++ config/params.py
@@ -9,7 +9,8 @@ Pins and ports
 '''
 
 kit_servo_pin = "P9_22"
-imu_serial_port = "/dev/ttyUSB0"
+imu_serial_port = "/dev/ttyO1"
+imu_baudrate = 115200
 
 tof_pins = {'60_NO' : 'gpio68', '60_NE' : 'gpio66', '60_SO' : 'gpio45', '60_SE' : 'gpio47', '60_EN' : 'gpio67', '60_ES' : 'gpio26', '60_ON' : 'gpio69', '60_OS' : 'gpio44', '200_N': 'gpio112', '200_S': 'gpio49', '200_E': 'gpio27', '200_O': 'gpio20'}
 tof_addresses = {'60_NO' : 0x20, '60_NE' : 0x21 ,'60_SO' : 0x22, '60_SE' : 0x23, '60_EN' : 0x24, '60_ES' : 0x25, '60_ON' : 0x26, '60_OS' : 0x27, '200_N': 0x30, '200_S': 0x31, '200_E': 0x32, '200_O': 0x33}
@@ -22,6 +23,7 @@ motors_pins = {'fl':'P8_13','fr':'P8_19','rl':'P9_14','rr':'P9_16','dir_fl':'gpi
 ToF calibration
 '''
 tof_calibration = {'NO' : 25, 'NE' : 25, 'SO' : 25, 'SE' : 25, 'EN' : 25, 'ES' : 25, 'ON' : 25, 'OS' : 25, 'N': 30, 'S': 30, 'E': 30, 'O': 30}
+is_there_a_wall_threshold = 200
 
 '''
 PID costants
--- sensors/imu.py
+++ sensors/imu.py
@@ -1,15 +1,29 @@
-import Adafruit_BBIO.UART as UART
+import sys
+sys.path.append("../")
+
 import serial
+import time
+import config.params as params
 
 yaw = 0
+yawsum = 0
 pitch = 0
 roll = 0
 
 class Imu:
-    def __init__(self, port = params.imu_serial_port):
-        self.ser = serial.Serial(port = port, baudrate=115200)
+    def __init__(self, port = params.imu_serial_port, baudrate = params.imu_baudrate):
+        self.ser = serial.Serial(port = port, baudrate=baudrate)
         self.ser.close()
         self.ser.open()
 
     def update(self):
-        print(self.ser.read())
+        if self.ser.isOpen():
+            ser.flushInput()
+            while ser.read() != '#':
+                pass
+            value = ser.readline()[4:][:-2].split(',');
+            value = [int(float(i)) for i in value]
+            yaw = value[0]
+            pitch = value[1]
+            roll = value[2]
+            yawsum = value[3]
