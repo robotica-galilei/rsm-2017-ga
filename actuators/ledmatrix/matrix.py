15639f1a1edce44e82e03033768d250db4fadad6
gottardini <danielegott98@gmail.com>
2017-07-07 12:55:44 +0200

    Display functions done

--- actuators/ledmatrix/matrix.py
+++ actuators/ledmatrix/matrix.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import time
 import maps
 from PIL import Image
@@ -7,65 +8,105 @@ display = BicolorMatrix8x8.BicolorMatrix8x8()
 display.begin()
 colors=[BicolorMatrix8x8.RED, BicolorMatrix8x8.GREEN, BicolorMatrix8x8.YELLOW]
 
+
+def flash(n_blink=2,freq=4):
+    period=1
+    if freq>0:
+        period=1/freq
+    for i in range(n_blink):
+        display.clear()
+        for x in range(8):
+            for y in range(8):
+                display.set_pixel(x,y,colors[0])
+        display.write_display()
+        time.sleep(period/2)
+        display.clear()
+        display.write_display()
+        time.sleep(period/2)
+
+
 def drawMap(map,color):
     c=colors[color]
     display.clear()
-    for x in range(8):
-        row=list(map[x])
+    for y in range(8):
+        row=list(map[y])
         print(row)
-        for y in range(8):
-            if row[y]=='1':
-                display.set_pixel(x, 7-y, c)
+        for x in range(8):
+            if row[x]=='1':
+                display.set_pixel(x, y, c)
     display.write_display()
 
-def drawWall(n,color):
+def drawWall(n,color=1):
     c=colors[color]
-    display.set_pixel(0,0,c)
+    walls=[((0,0),(0,7)),((0,7),(7,7)),((7,7),(0,7)),((0,7),(0,0))]
+    selected_wall=walls[n]
+    for y in range(selected_wall[1][0],selected_wall[1][1]+1):
+        for x in range(selected_wall[0][0],selected_wall[0][1]+1):
+            display.set_pixel(x,y,c)
     display.write_display()
 
+def sigTime(minutes):
+    if minutes<0 or minutes >8:
+        return -1
+    display.clear()
+    display.write_display()
+    drawMap(maps.numbers[minutes],(0 if minutes<=2 else 2) if minutes<=4 else 1)
 
-def goForward():
-    drawMap(maps.fwd,1)
+def sigLost():
+    display.clear()
+    display.write_display()
+    drawMap(maps.lost,0)
 
-def goLeft():
-    drawMap(maps.left,1)
+def sigHome(status=0):
+    display.clear()
+    display.write_display()
+    drawMap(maps.home,status)
 
-def goRight():
-    drawMap(maps.right,1)
+def sigWalls(walls):
+    display.clear()
+    display.write_display()
+    for i in range(4):
+        if walls[i]==1:
+            drawWall(i)
 
-def goBack():
-    drawMap(maps.back,1)
 
-def goHome():
-    drawMap(maps.home,2)
+def sigDirection(dir,status=1):
+    display.clear()
+    display.write_display()
+    arrow_maps=[maps.left,maps.back,maps.right,maps.fwd]
+    drawMap(arrow_maps[dir],2 if status==1 else 1)
 
-def gotHome():
-    drawMap(maps.home,1)
 
-def hotVictim():
-    drawMap(maps.heat,2)
+def sigVictim(victimtype=None,side=None):
+    display.clear()
+    display.write_display()
+    flash()
+    if victimtype!=None:
+        vic_maps={'O':maps.heat,'H':maps.visH,'S':maps.visS,'U':maps.visU}
+        drawMap(vic_maps[victimtype],0)
+        if side!=None:
+            drawWall(side)
 
-def unharmedVictim():
-    drawMap(maps.visU,2)
+while(True):
+    display.clear()
+    display.write_display()
+    raw_input("Continue")
+    for i in range(9):
+        sigTime(i)
+        time.sleep(1)
+    sigLost()
+    time.sleep(2)
+    sigHome(1)
+    time.sleep(1)
+    wall_list=[1,1,1,1]
+    sigWalls(wall_list)
+    time.sleep(2)
+    vict_list=['O','H','S','U']
+    for i in range(4):
+        sigDirection(i,2)
+        time.sleep(1)
 
-def harmedVictim():
-    drawMap(maps.visH,2)
-
-def stableVictim():
-    drawMap(maps.visS,2)
-
-"""
-goForward()
-time.sleep(1)
-goLeft()
-time.sleep(1)
-goRight()
-time.sleep(1)
-goBack()
-time.sleep(1)
-goHome()
-time.sleep(1)
-gotHome()
-"""
-stableVictim()
-drawWall(0,0)
+    for i in range(4):
+        for j in range(4):
+            sigVictim(vict_list[i],j)
+            time.sleep(1)
