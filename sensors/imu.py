237c570a0edc8ca31a186030d09bc093a09d6261
ballerin <francesco@ballerin.it>
2017-07-21 14:35:32 +0200

    Added functions to retrieve values

--- sensors/imu.py
+++ sensors/imu.py
@@ -15,7 +15,7 @@ roll = 0
 class Imu:
     def __init__(self, port = params.imu_serial_port, baudrate = params.imu_baudrate, from_ros=False):
         self.from_ros = from_ros
-        if not from_ros:
+        if not self.from_ros:
             self.ser = serial.Serial(port = port, baudrate=baudrate, timeout=1)
             self.ser.close()
             self.ser.open()
@@ -37,6 +37,18 @@ class Imu:
         self.roll = int(self.roll)
         self.yawsum = int(self.yawsum)
         self.last_time = time.time()
+
+    def get_yawsum(self):
+        return int(self.yawsum)
+
+    def get_roll(self):
+        return int(self.roll)
+
+    def get_pitch(self):
+        return int(self.pitch)
+
+    def get_yaw(self):
+        return int(self.yaw)
 
     def update(self):
         if not self.from_ros:
