462d27e2cb56e83ec5c0f647cb5fa9d4a84ea893
ballerin <francesco@ballerin.it>
2017-07-21 14:37:38 +0200

    Autostart bug fix and stdout redirected to /dev/null

--- parti_ti_prego.py
+++ parti_ti_prego.py
@@ -11,12 +11,14 @@ if __name__ == '__main__':
     pin = "gpio65"
     #os.system("sh /root/rsm-2017-ga/start.sh")
     #os.system("sh /root/rsm-2017-ga/start_routine.sh")
+    os_command = "python /root/rsm-2017-ga/main.py"
 
     GPIO.setup(pin, GPIO.IN)
     m = motors.Motor(params.motors_pins)
     m.stop()
-    pro = subprocess.Popen("python /root/rsm-2017-ga/main.py", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-
+    pro = subprocess.Popen(os_command, stdout=open(os.devnull, 'wb'), shell=True, preexec_fn=os.setsid)
+    print(os.getpgid(pro.pid))
+    print(pro.pid)
     while True:
         if GPIO.input(pin) == True:
             started_pressing = time.time()
@@ -25,11 +27,12 @@ if __name__ == '__main__':
             pressed_time = time.time() - started_pressing
             print("OMFG, you pressed da button!")
             if pressed_time < 3:
-                os.killpg(os.getpgid(pro.pid), signal.SIGKILL)
+                #os.killpg(os.getpgid(pro.pid), signal.SIGKILL)
+                os.system("pkill -9 -f \"SCREEN -m -d bash -c python /root/rsm-2017-ga/main.py\"")
                 m.stop()
                 #os.system("kill -9 " + os.getpgid(pro.pid))
                 time.sleep(1)
-                pro = subprocess.Popen("python /root/rsm-2017-ga/main.py", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+                pro = subprocess.Popen(os_command, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
             else:
                 os.system("killall -9 screen")
                 os.system("killall -9 python")
